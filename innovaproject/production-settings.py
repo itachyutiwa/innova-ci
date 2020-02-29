b978e4a6e2ad78f3233698abe3feb81a1435fc37
itachyutiwa <54214135+itachyutiwa@users.noreply.github.com>
2020-02-29 20:33:03 +0000

    Ajout du package whitenoise pour la stylisation en production

--- innovaproject/production-settings.py
+++ innovaproject/production-settings.py
@@ -1,6 +1,10 @@
+import dj_database_url
 from .settings import *
 
 DEBUG=False
 TEMPLATE_DEUG=False
+DATABASES['default'] = dj_database_url.config()
+MIDDLEWARE+=['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECRET_KEY = 'n$i73dz)qq9-@4de)erk^%^d#7z7c4da$uw5!2+&dh10p&$&07'
 ALLOWED_HOSTS = ['innova-ci.herokuapp.com']
